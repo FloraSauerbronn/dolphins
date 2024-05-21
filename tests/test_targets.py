91c1bc2bf810fc017da5c38831f92d272cd86bac
Pedro Igor <pedroigorao@hotmail.com>
2024-05-20 22:19:46 -0300

    move test data to csv samples

--- tests/test_targets.py
+++ tests/test_targets.py
@@ -1,99 +1,25 @@
 import pandas as pd
+import pytest
 from pandas.testing import assert_frame_equal
+from pathlib import Path
 
 from dolphins.targets import join_target
 
 
-def test_join_target_chunk_contains_percentage_call():
-    audio_metadata_df = pd.DataFrame(
-        {
-            "audio_filename": [
-                "audio1.wav",
-            ],
-            "original_file_length_seconds": [
-                365.4,
-            ],
-            "original_file_sample_rate": [
-                96_000,
-            ],
-            "chunk_index": [
-                0,
-            ],
-            "chunk_start_seconds": [
-                0.0,
-            ],
-            "chunk_end_seconds": [
-                2.0,
-            ],
-            "chunk_file_name": [
-                "dolphins/audios/audio1/chunk_0.wav",
-            ],
-        }
+@pytest.mark.parametrize(
+    "join_stategy_name",
+    [
+        "chunk_contains_percentage_call",
+    ],
+)
+def test_join_target(join_stategy_name: str):
+    fixtures_path: Path = Path("tests") / "fixtures" / "join_target" / join_stategy_name
+    audio_metadata_df: pd.DataFrame = pd.read_csv(fixtures_path / "audio_metadata.csv")
+    labels_df: pd.DataFrame = pd.read_csv(fixtures_path / "labels.csv")
+    actual: pd.DataFrame = join_target(
+        audio_metadata_df=audio_metadata_df,
+        labels_df=labels_df,
+        join_stategy_name="chunk_contains_percentage_call",
     )
-    labels_df = pd.DataFrame(
-        {
-            "audio_filename": [
-                "audio1.wav",
-            ],
-            "call_channel": [
-                1,
-            ],
-            "label": [
-                "click",
-            ],
-            "call_begin_time": [
-                0.0,
-            ],
-            "call_end_time": [
-                0.5,
-            ],
-            "call_length_seconds": [
-                0.5,
-            ],
-        }
-    )
-    actual = join_target(audio_metadata_df, labels_df, "chunk_contains_percentage_call")
-    expected = pd.DataFrame(
-        {
-            "audio_filename": [
-                "audio1.wav",
-            ],
-            "original_file_length_seconds": [
-                365.4,
-            ],
-            "original_file_sample_rate": [
-                96_000,
-            ],
-            "chunk_index": [
-                0,
-            ],
-            "chunk_start_seconds": [
-                0.0,
-            ],
-            "chunk_end_seconds": [
-                2.0,
-            ],
-            "chunk_file_name": [
-                "dolphins/audios/audio1/chunk_0.wav",
-            ],
-            "call_channel": [
-                1,
-            ],
-            "label": [
-                "click",
-            ],
-            "call_begin_time": [
-                0.0,
-            ],
-            "call_end_time": [
-                0.5,
-            ],
-            "call_length_seconds": [
-                0.5,
-            ],
-            "call_lenght_within_chunk": [
-                0.5,
-            ],
-        }
-    )
+    expected: pd.DataFrame = pd.read_csv(fixtures_path / "expected.csv")
     assert_frame_equal(actual, expected, check_like=True)
