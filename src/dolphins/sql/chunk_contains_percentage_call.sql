f44bd1419481fe39a67aa2c12640cfd00ae7ae76
Pedro Igor <pedroigorao@hotmail.com>
2024-12-07 20:38:19 -0300

    extend tests

--- src/dolphins/sql/chunk_contains_percentage_call.sql
+++ src/dolphins/sql/chunk_contains_percentage_call.sql
@@ -1,44 +1,47 @@
 -- Table of valid chunks with dolphin calls
 SELECT *
 FROM (
-    SELECT
-        am.*,
-        l.label AS label,
-        l.call_begin_time AS call_begin_time,
-        l.call_end_time AS call_end_time,
-        l.call_length_seconds AS call_length_seconds,
-        CASE
-            WHEN (
-                l.call_begin_time < am.chunk_end_seconds AND
-                am.chunk_end_seconds < l.call_end_time
-            ) THEN (am.chunk_end_seconds - GREATEST(l.call_begin_time, am.chunk_start_seconds))
-            WHEN (
-                l.call_begin_time < am.chunk_start_seconds AND
-                am.chunk_start_seconds < l.call_end_time
-            ) THEN (LEAST(l.call_end_time, am.chunk_end_seconds) - am.chunk_start_seconds)
-            ELSE
-                l.call_length_seconds END AS call_length_within_chunk
-    FROM
-        audio_metadata_df AS am INNER JOIN
-        labels_df AS l 
-            ON (( --  Keep chunks that contain a call within its boundaries
-                am.audio_filename = l.audio_filename AND
-                am.channel = l.channel AND
-                am.chunk_start_seconds <= l.call_begin_time AND
-                l.call_end_time <= am.chunk_end_seconds
-            ) OR ( -- Keep chunks that contain only the ending of a call
-                am.audio_filename = l.audio_filename AND
-                am.channel = l.channel AND
-                l.call_begin_time < am.chunk_start_seconds AND
-                am.chunk_start_seconds < l.call_end_time
-            ) OR ( -- Keep chunks that contain only the beginning of a call
-                am.audio_filename = l.audio_filename AND
-                am.channel = l.channel AND
-                l.call_begin_time < am.chunk_end_seconds AND
-                am.chunk_end_seconds < l.call_end_time
-            ))
+    SELECT *
+    FROM (
+        SELECT
+            am.*,
+            l.label AS label,
+            l.call_begin_time AS call_begin_time,
+            l.call_end_time AS call_end_time,
+            l.call_length_seconds AS call_length_seconds,
+            CASE
+                WHEN (
+                    l.call_begin_time < am.chunk_end_seconds AND
+                    am.chunk_end_seconds < l.call_end_time
+                ) THEN (am.chunk_end_seconds - GREATEST(l.call_begin_time, am.chunk_start_seconds))
+                WHEN (
+                    l.call_begin_time < am.chunk_start_seconds AND
+                    am.chunk_start_seconds < l.call_end_time
+                ) THEN (LEAST(l.call_end_time, am.chunk_end_seconds) - am.chunk_start_seconds)
+                ELSE
+                    l.call_length_seconds END AS call_length_within_chunk
+        FROM
+            audio_metadata_df AS am INNER JOIN
+            labels_df AS l 
+                ON (( --  Keep chunks that contain a call within its boundaries
+                    am.audio_filename = l.audio_filename AND
+                    am.channel = l.channel AND
+                    am.chunk_start_seconds <= l.call_begin_time AND
+                    l.call_end_time <= am.chunk_end_seconds
+                ) OR ( -- Keep chunks that contain only the ending of a call
+                    am.audio_filename = l.audio_filename AND
+                    am.channel = l.channel AND
+                    l.call_begin_time < am.chunk_start_seconds AND
+                    am.chunk_start_seconds < l.call_end_time
+                ) OR ( -- Keep chunks that contain only the beginning of a call
+                    am.audio_filename = l.audio_filename AND
+                    am.channel = l.channel AND
+                    l.call_begin_time < am.chunk_end_seconds AND
+                    am.chunk_end_seconds < l.call_end_time
+                ))
+    ) AS t
     QUALIFY
-        row_number() OVER (PARTITION BY am.audio_filename, am.chunk_index, am.channel ORDER BY l.call_begin_time) = 1
+        row_number() OVER (PARTITION BY t.audio_filename, t.chunk_index, t.channel ORDER BY t.call_length_within_chunk DESC NULLS LAST) = 1
 ) AS t
 WHERE
     t.call_length_seconds = t.call_length_within_chunk OR -- 100% of call length within chunk to consider small calls
